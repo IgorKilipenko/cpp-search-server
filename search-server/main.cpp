a898d4ec0f396094a90d21f45842f3c6631671f4
IgorKilipenko <igorkili@mail.ru>
2022-06-20 13:09:40 +0700

    initial sprint_1 examination

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
-#include <string>
 #include <set>
+#include <string>
 
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь: 271
+#include "server.hpp"
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
 
-int main()
-{
-    const char findNumber = '3';
-    int result = 0;
-    for (int i = 1; i <= 1000; i++)
-    {
-        std::string str = std::to_string(i);
-        const std::set<char> buff(str.begin(), str.end());
-        if (buff.count(findNumber))
-        {
-            ++result;
-            std::cout << "number: " << i << " contains " << findNumber << std::endl;
-        }
+int ReadLineWithNumber() {
+    int result;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+void PrintDocument(const Document& document) {
+    cout << "{ "s
+         << "document_id = "s << document.id << ", "s
+         << "relevance = "s << document.relevance << ", "s
+         << "rating = "s << document.rating << " }"s << endl;
+}
+
+int main() {
+    SearchServer search_server;
+    search_server.SetStopWords("и в на"s);
+
+    search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, -3});
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
+    search_server.AddDocument(3, "ухоженный скворец евгений"s, DocumentStatus::BANNED, {9});
+
+    cout << "ACTUAL by default:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s)) {
+        PrintDocument(document);
     }
-    std::cout << std::endl;
-    std::cout << result << " numbers contains " << findNumber << std::endl
-              << std::endl;
+
+    cout << "ACTUAL:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments(
+             "пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return status == DocumentStatus::ACTUAL; })) {
+        PrintDocument(document);
+    }
+
+    cout << "Even ids:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments(
+             "пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
+        PrintDocument(document);
+    }
     return 0;
 }
