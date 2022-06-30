24dcbaa50249f3198c94df06f75b1b7f5e95215e
IgorKilipenko <igorkili@mail.ru>
2022-07-01 02:37:49 +0700

    solve s4_topic_22_les9/1.1 (stack and queue)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "server.hpp"
+#include "request_queue.hpp"
 
 string ReadLine() {
     string s;
@@ -75,31 +76,25 @@ auto Paginate(const Container<T>& c, size_t page_size) {
 }
 
 int main() {
-    SearchServer search_server("and with"s);
+    SearchServer search_server("and in at"s);
+    RequestQueue request_queue(search_server);
 
-    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
 
-    search_server.AddDocument(6, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(7, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(8, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(9, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(10, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(11, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-    search_server.AddDocument(12, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-
-    const auto search_results = search_server.FindTopDocuments("curly dog"s);
-    int page_size = 4;
-    const auto pages = Paginate(search_results, page_size);
-
-    // Выводим найденные документы по страницам
-    for (auto page = pages.begin(); page != pages.end(); ++page) {
-        cout << *page << endl;
-        cout << "Page break"s << endl;
+    // 1439 запросов с нулевым результатом
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty request"s);
     }
-
+    // все еще 1439 запросов с нулевым результатом
+    request_queue.AddFindRequest("curly dog"s);
+    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
+    request_queue.AddFindRequest("big collar"s);
+    // первый запрос удален, 1437 запросов с нулевым результатом
+    request_queue.AddFindRequest("sparrow"s);
+    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
     return 0;
-}
+} 
