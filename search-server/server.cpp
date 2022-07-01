6dc164617cb37c102b3b217e6b8718d05d913fc9
IgorKilipenko <igorkili@mail.ru>
2022-07-01 09:33:58 +0700

    fix server fom src

--- search-server/server.cpp
+++ search-server/server.cpp
@@ -101,6 +101,9 @@ bool SearchServer::IsStopWord(const string& word) const {
 vector<string> SearchServer::SplitIntoWordsNoStop(const string& text) const {
     vector<string> words;
     for (const string& word : SplitIntoWords(text)) {
+        if (!IsValidWord(word)) {
+            throw invalid_argument("Word "s + word + " is invalid"s);
+        }
         if (!IsStopWord(word)) {
             words.push_back(word);
         }
