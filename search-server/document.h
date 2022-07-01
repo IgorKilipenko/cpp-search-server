8fd22e69248676743bf3539dd7cb83178a0846b8
IgorKilipenko <igorkili@mail.ru>
2022-07-01 07:07:33 +0700

    fix print document (comma sep)

--- search-server/document.h
+++ search-server/document.h
@@ -10,9 +10,12 @@ struct Document {
     int rating;
     Document() : id{0}, relevance{0.0}, rating{0} {}
     Document(int id, double relevance, int rating) : id{id}, relevance{relevance}, rating{rating} {}
-    friend ostream& operator<<(ostream& os, const Document& doc) {
-        os << "{ document_id = " << doc.id << " relevance = " << doc.relevance << " rating = " << doc.rating << " }";
-        return os;
+    friend ostream& operator<<(ostream& out, const Document& document) {
+        out << "{ "s
+            << "document_id = "s << document.id << ", "s
+            << "relevance = "s << document.relevance << ", "s
+            << "rating = "s << document.rating << " }"s;
+        return out;
     }
 };
 
