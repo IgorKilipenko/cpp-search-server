0353693397c228ee4bd0308f08c841fa71812748
IgorKilipenko <igorkili@mail.ru>
2022-07-05 11:08:45 +0700

    TicketOffice training

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,75 +1,83 @@
 #include <algorithm>
-#include <cstddef>
-#include <functional>
+#include <deque>
 #include <iostream>
 #include <string>
-#include <vector>
-
-#include "log_duration.h"
 
 using namespace std;
 
-template <typename TestFunc = function<bool(const string&)>>
-class Bruteforce {
+struct Ticket {
+    int id = 0;
+    string name;
+};
+
+class TicketOffice {
    public:
-    Bruteforce(TestFunc check) : custom_check_function_{check} {}
-    string Generate(const string& input) {
-        password_ = ""s;
-        string result(input.size(), input[0]);
-        vector<int> index(input.size(), 0);
-
-        for (int length = 1; length <= input.size(); length++) {
-            int updateIndex = 0;
-            do {
-                if (Test(result, 0, length)) {
-                    return password_;
-                }
-
-                for (updateIndex = length - 1; updateIndex != -1 && ++index[updateIndex] == input.size();
-                     result[updateIndex] = input[0], index[updateIndex] = 0, updateIndex--) {
-                }
-
-                if (updateIndex != -1) result[updateIndex] = input[index[updateIndex]];
-            } while (updateIndex != -1);
-        }
-        return password_;
+    // добавить билет в систему
+    void PushTicket(const string& name) {
+        // реализуйте метод
+        if (first_id_ < 0) first_id_ = 0;
+        ++last_id_;
+        ++size_;
+        tickets_.push_back({last_id_, name});
     }
-    string GetPassword() const {
-        return password_;
+
+    // получить количество доступных билетов
+    int GetAvailable() const {
+        // реализуйте метод
+        return size_;
+    }
+
+    // получить количество доступных билетов определённого типа
+    int GetAvailable(const string& name) const {
+        // реализуйте метод
+        int result = count_if(tickets_.begin(), tickets_.end(), [&name](const Ticket& t) {
+            return t.name == name;
+        });
+        return result;
+    }
+
+    // отозвать старые билеты (до определённого id)
+    void Invalidate(int minimum) {
+        // реализуйте метод
+        int size = min(minimum, last_id_ + 1);
+        for (int i = first_id_; i < size; ++i) {
+            tickets_.pop_front();
+            --size_;
+            first_id_++;
+        }
     }
 
    private:
-    string password_;
-    TestFunc custom_check_function_;
-    bool Test(const string& result, int offset, int length) {
-        string text = result.substr(offset, length);
-        if (custom_check_function_(text)) {
-            password_ = text;
-            return true;
-        }
-        return false;
-    }
+    int last_id_ = -1;
+    int first_id_ = -1;
+    int size_ = 0;
+    deque<Ticket> tickets_;
 };
 
-template <typename F>
-string BruteForce(F check) {
-    // верните строку str, для которой check(str) будет true
-    int start_ch = static_cast<int>('A');
-    int end_ch = static_cast<int>('Z') + 1;
-    string chars;
-    chars.reserve(end_ch - start_ch);
-    for (int i = start_ch; i < end_ch; i++) {
-        chars.push_back(static_cast<char>(i));
-    }
-    Bruteforce bruteforce(check);
-    const string password = bruteforce.Generate(chars);
-    return password;
-}
-
 int main() {
-    string pass = "ARTUR"s;
-    auto check = [pass](const string& s) {
-        return s == pass;
-    };
-    cout << BruteForce(check) << endl;
+    TicketOffice tickets;
+
+    tickets.PushTicket("Swan Lake");      // id - 0
+    tickets.PushTicket("Swan Lake");      // id - 1
+    tickets.PushTicket("Boris Godunov");  // id - 2
+    tickets.PushTicket("Boris Godunov");  // id - 3
+    tickets.PushTicket("Swan Lake");      // id - 4
+    tickets.PushTicket("Boris Godunov");  // id - 5
+    tickets.PushTicket("Boris Godunov");  // id - 6
+
+    cout << tickets.GetAvailable() << endl;                 // Вывод: 7
+    cout << tickets.GetAvailable("Swan Lake") << endl;      // Вывод: 3
+    cout << tickets.GetAvailable("Boris Godunov") << endl;  // Вывод: 4
+
+    // Invalidate удалит билеты с номерами 0, 1, 2:
+    tickets.Invalidate(3);
+
+    cout << tickets.GetAvailable() << endl;                 // Вывод: 4
+    cout << tickets.GetAvailable("Swan Lake") << endl;      // Вывод: 1
+    cout << tickets.GetAvailable("Boris Godunov") << endl;  // Вывод: 3
+
+    tickets.PushTicket("Swan Lake");  // id - 7
+    tickets.PushTicket("Swan Lake");  // id - 8
+
+    cout << tickets.GetAvailable("Swan Lake") << endl;  // Вывод: 3
 }
