30724984707487c6ba3459ced4053573b1303f15
IgorKilipenko <igorkili@mail.ru>
2022-07-05 08:20:28 +0700

    Bruteforce

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,32 +1,74 @@
+#include <algorithm>
+#include <cstddef>
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include "document.h"
-#include "request_queue.h"
-#include "search_server.h"
+#include "log_duration.h"
 
 using namespace std;
 
-int main() {
-    SearchServer search_server("and in at"s);
-    RequestQueue request_queue(search_server);
 
-    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
+class Bruteforce {
+   public:
+    template <typename F>
+    const string& generate(const string& input, F check) {
+        result_ = ""s;
+        string result(input.size(), input[0]);
+        vector<int> index(input.size(), 0);
 
-    // 1439 запросов с нулевым результатом
-    for (int i = 0; i < 1439; ++i) {
-        request_queue.AddFindRequest("empty request"s);
+        // loop over the output lengths.
+        for (int length = 1; length <= input.size(); length++) {
+            int updateIndex = 0;
+            do {
+                if (element(result, 0, length, check)) {
+                    return result_;
+                }
+                // update values that need to reset.
+                for (updateIndex = length - 1; updateIndex != -1 && ++index[updateIndex] == input.size();
+                     result[updateIndex] = input[0], index[updateIndex] = 0, updateIndex--)
+                    ;
+
+                // update the character that is not resetting, if valid
+                if (updateIndex != -1) result[updateIndex] = input[index[updateIndex]];
+            } while (updateIndex != -1);
+        }
+        return result_;
     }
-    // все еще 1439 запросов с нулевым результатом
-    request_queue.AddFindRequest("curly dog"s);
-    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
-    request_queue.AddFindRequest("big collar"s);
-    // первый запрос удален, 1437 запросов с нулевым результатом
-    request_queue.AddFindRequest("sparrow"s);
-    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
-    return 0;
+    template <typename F>
+    bool element(const string& result, int offset, int length, F check) {
+        string text = result.substr(offset, length);
+        //cout << text << endl;
+        if (check(text)) {
+            result_ = text;
+            return true;
+        }
+        return false;
+    }
+
+   private:
+    string result_;
+};
+
+template <typename F>
+string BruteForce(F check) {
+    // верните строку str, для которой check(str) будет true
+    int start_ch = static_cast<int>('A');
+    int end_ch = static_cast<int>('Z') + 1;
+    string chars;
+    chars.reserve(end_ch - start_ch);
+    for (int i = start_ch; i < end_ch; i++) {
+        chars.push_back(static_cast<char>(i));
+    }
+    Bruteforce bruteforce;
+    const string password = bruteforce.generate(chars, check);
+    return password;
+}
+
+int main() {
+    string pass = "ARTUR"s;
+    auto check = [pass](const string& s) {
+        return s == pass;
+    };
+    cout << BruteForce(check) << endl;
 }
