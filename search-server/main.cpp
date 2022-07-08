e7ee5bbf4b10b476d16058ef51f055b0f528e25c
IgorKilipenko <igorkili@mail.ru>
2022-07-08 18:39:16 +0700

    solve traning ArrayPtr

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,25 +1,95 @@
 #include <cassert>
-#include <numeric>
-#include <vector>
+#include <cstddef>
+#include <cstdlib>
 
-template <typename T>
-void ReverseArray(T* start, size_t size) {
-    // Напишите тело функции самостоятельно
-    if (size == 0 || start == nullptr) return;
-    T* end = start + size - 1;
-    for (int i = 0; i < size / 2; ++i) {
-        T tmp = *(end - i);
-        *(end - i) = *(start + i);
-        *(start + i) = tmp;
+template <typename Type>
+class ArrayPtr {
+   public:
+    // Инициализирует ArrayPtr нулевым указателем
+    ArrayPtr() = default;
+
+    // Создаёт в куче массив из size элементов типа Type.
+    // Если size == 0, поле raw_ptr_ должно быть равно nullptr
+    explicit ArrayPtr(size_t size) {
+        if (size != 0) {
+            raw_ptr_ = new Type[size];
+        }
     }
-}
+
+    // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
+    explicit ArrayPtr(Type* raw_ptr) noexcept {
+        raw_ptr_ = raw_ptr;
+    }
+
+    // Запрещаем копирование
+    ArrayPtr(const ArrayPtr&) = delete;
+
+    ~ArrayPtr() {
+        delete[] raw_ptr_;
+    }
+
+    // Запрещаем присваивание
+    ArrayPtr& operator=(const ArrayPtr&) = delete;
+
+    // Прекращает владением массивом в памяти, возвращает значение адреса массива
+    // После вызова метода указатель на массив должен обнулиться
+    [[nodiscard]] Type* Release() noexcept {
+        Type* data = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return data;
+    }
+
+    // Возвращает ссылку на элемент массива с индексом index
+    Type& operator[](size_t index) noexcept {
+        return raw_ptr_[index];
+    }
+
+    // Возвращает константную ссылку на элемент массива с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        return raw_ptr_[index];
+    }
+
+    // Возвращает true, если указатель ненулевой, и false в противном случае
+    explicit operator bool() const {
+        return raw_ptr_ != nullptr;
+    }
+
+    // Возвращает значение сырого указателя, хранящего адрес начала массива
+    Type* Get() const noexcept {
+        return raw_ptr_;
+    }
+
+    // Обменивается значениям указателя на массив с объектом other
+    void swap(ArrayPtr& other) noexcept {
+        ArrayPtr::swap(&raw_ptr_, &(other.raw_ptr_));
+    }
+
+    // Обменивается значениям указателя на массив с объектом other
+    static void swap(Type** ptr1, Type** ptr2) noexcept {
+        Type* tmp = *ptr1;
+        *ptr1 = *ptr2;
+        *ptr2 = tmp;
+    }
+
+   private:
+    Type* raw_ptr_ = nullptr;
+};
 
 int main() {
-    using namespace std;
+    ArrayPtr<int> numbers(10);
+    const auto& const_numbers = numbers;
 
-    vector<int> v(0);
-    iota(v.begin(), v.end(), 1);
-    vector<int> expected_v(v.rbegin(), v.rend());
-    ReverseArray(v.data(), v.size());
-    assert(v == (expected_v));
+    numbers[2] = 42;
+    assert(const_numbers[2] == 42);
+    assert(&const_numbers[2] == &numbers[2]);
+
+    assert(numbers.Get() == &numbers[0]);
+
+    ArrayPtr<int> numbers_2(5);
+    numbers_2[2] = 43;
+
+    numbers.swap(numbers_2);
+
+    assert(numbers_2[2] == 42);
+    assert(numbers[2] == 43);
 }
