c0da001d6d0f8b12b619d9d6091db5ceef601e20
IgorKilipenko <igorkili@mail.ru>
2022-07-10 21:58:05 +0700

    solve training Editor

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,69 +1,107 @@
-#include "list.h"
-
 #include <cassert>
+#include <cmath>
+#include <iostream>
+#include <list>
 #include <vector>
 
 using namespace std;
 
-// функция возвращает функциональный объект,
-// выполняющий вставку в выбранный список
-template <class Type>
-auto MakeInsertingFunction(vector<SingleLinkedList<Type>>& lists, int x) {
-    const auto result = [&](const Type& value) {
-        lists[x].PushFront(value);
-    };
-    return result;
-}
-
-template <class T>
-void InsertRange(int from, int to, T push_function) {
-    for (int i = from; i < to; ++i) {
-        push_function(i);
+class Editor {
+   public:
+    using Iterator = std::list<char>::iterator;
+    Editor() : cursor_{text_.begin()} {}
+    // сдвинуть курсор влево
+    void Left() {
+        // assert(!text_.empty() && cursor_ != text_.begin());
+        if (cursor_ == text_.begin()) {
+            return;
+        }
+        --cursor_;
     }
-}
+    // сдвинуть курсор вправо
+    void Right() {
+        // assert(!text_.empty() && cursor_ != text_.end());
+        if (cursor_ == text_.end()) {
+            return;
+        }
+        ++cursor_;
+    }
+    // вставить символ token
+    void Insert(char token) {
+        cursor_ = text_.insert(cursor_, token);
+        ++cursor_;
+    }
+    // вырезать не более tokens символов, начиная с текущей позиции курсора
+    void Cut(size_t tokens = 1) {
+        buffer_.clear();
+        auto ptr = cursor_;
+        for (; ptr != text_.end() && tokens > 0; --tokens) {
+            buffer_.push_back(*ptr);
+            ptr = text_.erase(ptr);
+        }
+        cursor_ = ptr;
+    }
+    void Copy(size_t tokens = 1) {
+        buffer_.clear();
+        for (auto ptr = cursor_; ptr != text_.end() && tokens > 0; --tokens, ++ptr) {
+            buffer_.push_back(*ptr);
+        }
+    }
+    // вставить содержимое буфера в текущую позицию курсора
+    void Paste() {
+        if (buffer_.empty()) {
+            return;
+        }
+        auto dest_ptr = cursor_;
+        for (auto src_ptr = buffer_.begin(); src_ptr != buffer_.end(); ++src_ptr) {
+            dest_ptr = text_.insert(dest_ptr, *src_ptr);
+            dest_ptr++;
+        }
+        // cursor_ = dest_ptr == text_.end() ? dest_ptr : next(dest_ptr);
+        // cursor_ = dest_ptr != cursor_ ? ++dest_ptr : dest_ptr;
+        cursor_ = dest_ptr;
+    }
+    // получить текущее содержимое текстового редактора
+    string GetText() const {
+        return string(text_.begin(), text_.end());
+    }
+
+   private:
+    list<char> buffer_;
+    list<char> text_;
+    Iterator cursor_;
+};
 
 int main() {
-    // main тестирует вектор, в нём нет ошибок.
-    // не меняйте код этой функции
-    // помимо этих тестов, список должен проходить все обычные тесты списка.
-    // Ищите ошибки в коде списка и
-    // других функциях этого файла
+    Editor editor;
+    const string text = "hello, world"s;
+    for (char c : text) {
+        editor.Insert(c);
+    }
+    // Текущее состояние редактора: `hello, world|`
+    for (size_t i = 0; i < text.size(); ++i) {
+        editor.Left();
+    }
+    // Текущее состояние редактора: `|hello, world`
+    editor.Cut(7);
+    // Текущее состояние редактора: `|world`
+    // в буфере обмена находится текст `hello, `
+    for (size_t i = 0; i < 5; ++i) {
+        editor.Right();
+    }
+    // Текущее состояние редактора: `world|`
+    editor.Insert(',');
+    editor.Insert(' ');
+    // Текущее состояние редактора: `world, |`
+    editor.Paste();
+    // Текущее состояние редактора: `world, hello, |`
+    editor.Left();
+    editor.Left();
+    //Текущее состояние редактора: `world, hello|, `
+    editor.Cut(3);  // Будут вырезаны 2 символа
+    // Текущее состояние редактора: `world, hello|`
+    cout << editor.GetText();
 
-    vector<SingleLinkedList<int>> lists_a(10);
-
-    auto push_to_2a = MakeInsertingFunction(lists_a, 2);
-    auto push_to_5a = MakeInsertingFunction(lists_a, 5);
-    auto push_to_7a = MakeInsertingFunction(lists_a, 7);
-
-    InsertRange(10, 12, push_to_2a);
-    InsertRange(12, 14, push_to_5a);
-    InsertRange(14, 16, push_to_7a);
-
-    assert(lists_a[2] == SingleLinkedList<int>({11, 10}));
-    assert(lists_a[5] == SingleLinkedList<int>({13, 12}));
-    assert(lists_a[7] == SingleLinkedList<int>({15, 14}));
-
-    vector<SingleLinkedList<int>> lists_b = lists_a;
-
-    auto push_to_2b = MakeInsertingFunction(lists_b, 2);
-    auto push_to_5b = MakeInsertingFunction(lists_b, 5);
-    auto push_to_7b = MakeInsertingFunction(lists_b, 7);
-
-    InsertRange(20, 22, push_to_2b);
-    InsertRange(22, 24, push_to_5b);
-    InsertRange(24, 26, push_to_7b);
-
-    assert(lists_b[2] == SingleLinkedList<int>({21, 20, 11, 10}));
-    assert(lists_b[5] == SingleLinkedList<int>({23, 22, 13, 12}));
-    assert(lists_b[7] == SingleLinkedList<int>({25, 24, 15, 14}));
-
-    lists_a[2].PopFront();
-    lists_a[5].InsertAfter(lists_a[5].begin(), 100);
-    lists_b[5].EraseAfter(next(lists_b[5].begin()));
-    lists_b[7].Clear();
-
-    assert(lists_a[2] == SingleLinkedList<int>({10}));
-    assert(lists_a[5] == SingleLinkedList<int>({13, 100, 12}));
-    /*assert(lists_b[5] == SingleLinkedList<int>({23, 22, 12}));
-    assert(lists_b[7] == SingleLinkedList<int>());*/
+    assert(editor.GetText() == "world, hello"s);
+    return 0;
 }
