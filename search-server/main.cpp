773b98c32ed8225394361a01622baaf7ec3b54a1
IgorKilipenko <igorkili@mail.ru>
2022-07-11 14:06:07 +0700

    solve training Translator

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,37 +1,60 @@
-#include <cassert>
-#include <cstddef>
-#include <iostream>
+#include <map>
+#include <set>
+#include <string>
 #include <string_view>
-#include <vector>
+
+#include "my_assert.h"
 
 using namespace std;
 
-vector<string_view> SplitIntoWordsView(string_view str) {
-    if (str.empty()) {
-        return {};
-    }
-    vector<string_view> result;
-    const int64_t pos_end = str.npos;
-    str.remove_prefix(min(str.find_first_not_of(" "), str.size()));
+class Translator {
+   public:
+    Translator() : words_{}, translated_words_{}, forward_translations_{}, backward_translations_{} {}
 
-    while (!str.empty()) {
-        int64_t space = str.find(' ', 0);
-        result.push_back(space == pos_end ? str.substr(0) : str.substr(0, space));
-        str.remove_prefix(min(str.find_first_not_of(" ", space), str.size()));
+    void Add(std::string_view source, std::string_view target) {
+        auto [src_ptr, _] = words_.emplace(source);
+        auto [target_ptr, __] = translated_words_.emplace(target);
+        forward_translations_.insert({*src_ptr, *target_ptr});
+        backward_translations_.insert({*target_ptr, *src_ptr});
     }
 
-    return result;
+    std::string_view TranslateForward(std::string_view source) const {
+        if (forward_translations_.empty() || !forward_translations_.count(source)) {
+            return Translator::INVALID_RESULT;
+        }
+
+        auto result = forward_translations_.at(source);
+        return result;
+    }
+
+    std::string_view TranslateBackward(std::string_view target) const {
+        if (backward_translations_.empty() || !backward_translations_.count(target)) {
+            return Translator::INVALID_RESULT;
+        }
+
+        auto result = backward_translations_.at(target);
+        return result;
+    }
+
+   private:
+    inline static const std::string INVALID_RESULT = ""s;
+    std::set<std::string> words_;
+    std::set<std::string> translated_words_;
+    std::map<std::string_view, std::string_view> forward_translations_;
+    std::map<std::string_view, std::string_view> backward_translations_;
+};
+
+void TestSimple() {
+    Translator translator;
+    translator.Add(string("okno"s), string("window"s));
+    translator.Add(string("stol"s), string("table"s));
+
+    assert(translator.TranslateForward("okno"s) == "window"s);
+    assert(translator.TranslateBackward("table"s) == "stol"s);
+    assert(translator.TranslateForward("table"s) == ""s);
 }
 
 int main() {
-    assert((SplitIntoWordsView("") == vector<string_view>{}));
-    assert((SplitIntoWordsView("     ") == vector<string_view>{}));
-    assert((SplitIntoWordsView("aaaaaaa") == vector{"aaaaaaa"sv}));
-    assert((SplitIntoWordsView("a") == vector{"a"sv}));
-    assert((SplitIntoWordsView("a b c") == vector{"a"sv, "b"sv, "c"sv}));
-    assert((SplitIntoWordsView("a    bbb   cc") == vector{"a"sv, "bbb"sv, "cc"sv}));
-    assert((SplitIntoWordsView("  a    bbb   cc") == vector{"a"sv, "bbb"sv, "cc"sv}));
-    assert((SplitIntoWordsView("a    bbb   cc   ") == vector{"a"sv, "bbb"sv, "cc"sv}));
-    assert((SplitIntoWordsView("  a    bbb   cc   ") == vector{"a"sv, "bbb"sv, "cc"sv}));
-    cout << "All OK" << endl;
+    TestSimple();
+    return 0;
 }
