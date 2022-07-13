c86fca4e5a9a74ce3973428f361df89b3d8d5d58
IgorKilipenko <igorkili@mail.ru>
2022-07-14 02:32:25 +0700

    solve training sprint8: CountWords (pthread)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,158 +1,51 @@
-#include <cassert>
+#include <algorithm>
 #include <iostream>
-#include <numeric>
+#include <random>
+#include <string>
+#include <string_view>
+#include <execution>
 
-#include "simple_vector.h"
+#include "log_duration.h"
 
 using namespace std;
 
-class X {
-   public:
-    X() : X(5) {}
-    X(size_t num) : x_(num) {}
-    X(const X& other) = delete;
-    X& operator=(const X& other) = delete;
-    X(X&& other) {
-        x_ = exchange(other.x_, 0);
+string GenerateQuery(mt19937& generator, int max_length, int space_rate) {
+    const int length = uniform_int_distribution(1, max_length)(generator);
+    string query(length, ' ');
+    for (char& c : query) {
+        const int rnd = uniform_int_distribution(0, space_rate - 1)(generator);
+        if (rnd > 0) {
+            c = 'a' + (rnd - 1);
+        }
     }
-    X& operator=(X&& other) {
-        x_ = exchange(other.x_, 0);
-        return *this;
-    }
-    size_t GetX() const {
-        return x_;
-    }
-
-   private:
-    size_t x_;
-};
-
-SimpleVector<int> GenerateVector(size_t size) {
-    SimpleVector<int> v(size);
-    iota(v.begin(), v.end(), 1);
-    return v;
+    return query;
 }
 
-void TestTemporaryObjConstructor() {
-    const size_t size = 1000000;
-    cout << "Test with temporary object, copy elision" << endl;
-    SimpleVector<int> moved_vector(GenerateVector(size));
-    assert(moved_vector.GetSize() == size);
-    cout << "Done!" << endl << endl;
-}
-
-void TestTemporaryObjOperator() {
-    const size_t size = 1000000;
-    cout << "Test with temporary object, operator=" << endl;
-    SimpleVector<int> moved_vector;
-    assert(moved_vector.GetSize() == 0);
-    moved_vector = GenerateVector(size);
-    assert(moved_vector.GetSize() == size);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNamedMoveConstructor() {
-    const size_t size = 1000000;
-    cout << "Test with named object, move constructor" << endl;
-    SimpleVector<int> vector_to_move(GenerateVector(size));
-    assert(vector_to_move.GetSize() == size);
-
-    SimpleVector<int> moved_vector(move(vector_to_move));
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNamedMoveOperator() {
-    const size_t size = 1000000;
-    cout << "Test with named object, operator=" << endl;
-    SimpleVector<int> vector_to_move(GenerateVector(size));
-    assert(vector_to_move.GetSize() == size);
-
-    SimpleVector<int> moved_vector = move(vector_to_move);
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableMoveConstructor() {
-    const size_t size = 5;
-    cout << "Test noncopiable object, move constructor" << endl;
-    SimpleVector<X> vector_to_move;
-    for (size_t i = 0; i < size; ++i) {
-        vector_to_move.PushBack(X(i));
+template <typename Solver>
+void Test(string_view mark, string_view s, Solver solver) {
+    int result;
+    {
+        LOG_DURATION(mark);
+        result = solver(s);
     }
-
-    SimpleVector<X> moved_vector = move(vector_to_move);
-    assert(moved_vector.GetSize() == size);
-    assert(vector_to_move.GetSize() == 0);
-
-    for (size_t i = 0; i < size; ++i) {
-        assert(moved_vector[i].GetX() == i);
-    }
-    cout << "Done!" << endl << endl;
+    cout << result << endl;
 }
 
-void TestNoncopiablePushBack() {
-    const size_t size = 5;
-    cout << "Test noncopiable push back" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
+#define TEST(solver) Test(#solver, s, solver)
 
-    assert(v.GetSize() == size);
-
-    for (size_t i = 0; i < size; ++i) {
-        assert(v[i].GetX() == i);
-    }
-    cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableInsert() {
-    const size_t size = 5;
-    cout << "Test noncopiable insert" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
-
-    // в начало
-    v.Insert(v.begin(), X(size + 1));
-    assert(v.GetSize() == size + 1);
-    assert(v.begin()->GetX() == size + 1);
-    // в конец
-    v.Insert(v.end(), X(size + 2));
-    assert(v.GetSize() == size + 2);
-    assert((v.end() - 1)->GetX() == size + 2);
-    // в середину
-    v.Insert(v.begin() + 3, X(size + 3));
-    assert(v.GetSize() == size + 3);
-    assert((v.begin() + 3)->GetX() == size + 3);
-    cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableErase() {
-    const size_t size = 3;
-    cout << "Test noncopiable erase" << endl;
-    SimpleVector<X> v;
-    for (size_t i = 0; i < size; ++i) {
-        v.PushBack(X(i));
-    }
-
-    auto it = v.Erase(v.begin());
-    assert(it->GetX() == 1);
-    cout << "Done!" << endl << endl;
+int CountWords(string_view str) {
+    return std::count(std::execution::par, str.begin(), str.end(), ' ') + 1;
 }
 
 int main() {
-    TestTemporaryObjConstructor();
-    TestTemporaryObjOperator();
-    TestNamedMoveConstructor();
-    TestNamedMoveOperator();
-    TestNoncopiableMoveConstructor();
-    TestNoncopiablePushBack();
-    TestNoncopiableInsert();
-    TestNoncopiableErase();
+    // должно вывести 3
+    cout << CountWords("pretty little octopus"sv) << endl;
+
+    mt19937 generator;
+
+    const string s = GenerateQuery(generator, 50'000'000, 4);
+
+    TEST(CountWords);
+
     return 0;
 }
