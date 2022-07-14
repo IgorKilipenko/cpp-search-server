7b9121ce11a44fd158714b75aa761451de789abc
IgorKilipenko <igorkili@mail.ru>
2022-07-14 18:45:07 +0700

    solve training sprint8: CountWords (1.1)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,16 +1,17 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
+#include <numeric>
 #include <random>
 #include <string>
 #include <string_view>
-#include <execution>
 
 #include "log_duration.h"
 
 using namespace std;
 
 string GenerateQuery(mt19937& generator, int max_length, int space_rate) {
-    const int length = uniform_int_distribution(1, max_length)(generator);
+    const int length = uniform_int_distribution(0, max_length)(generator);
     string query(length, ' ');
     for (char& c : query) {
         const int rnd = uniform_int_distribution(0, space_rate - 1)(generator);
@@ -34,16 +35,35 @@ void Test(string_view mark, string_view s, Solver solver) {
 #define TEST(solver) Test(#solver, s, solver)
 
 int CountWords(string_view str) {
-    return std::count(std::execution::par, str.begin(), str.end(), ' ') + 1;
+    // подсчитайте количество слов,
+    // игнорируя начальные, конечные
+    // и подряд идущие пробелы
+
+    // некорректное решение, с которым сравнивается производительность
+    if (str.empty()) {
+        return 0;
+    }
+    auto ptr = std::find_if(str.begin(), str.end(), [](const char c) {
+        return c != ' ';
+    });
+    if (ptr == str.end()) {
+        return 0;
+    }
+    size_t size = transform_reduce(ptr, prev(str.end()), next(ptr), 0ul, std::plus<>{},
+                                   [](const char cur, const char next) {
+                                       return cur != ' ' && next == ' ';
+                                   }) +
+                  (*prev(str.end()) != ' ');
+    return static_cast<int>(size);
 }
 
 int main() {
     // должно вывести 3
-    cout << CountWords("pretty little octopus"sv) << endl;
+    cout << CountWords("  pretty  little octopus "sv) << endl;
 
     mt19937 generator;
 
-    const string s = GenerateQuery(generator, 50'000'000, 4);
+    const string s = GenerateQuery(generator, 100'000'000, 4);
 
     TEST(CountWords);
 
