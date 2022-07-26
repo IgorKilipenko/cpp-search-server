d931433d816c2f4dd8216f236010b91278808cee
IgorKilipenko <igorkili@mail.ru>
2022-07-26 18:58:39 +0700

    start solve training sprint8: ConcurrentMap (1.1 mutex)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,113 +1,184 @@
 #include <algorithm>
 #include <atomic>
+#include <cstddef>
+#include <cstdlib>
 #include <execution>
-#include <iostream>
+#include <future>
 #include <map>
 #include <mutex>
 #include <numeric>
 #include <random>
 #include <string>
-#include <string_view>
 #include <vector>
 
 #include "log_duration.h"
+#include "test_framework.h"
+
+using namespace std::string_literals;
+
+template <typename Key, typename Value>
+class ConcurrentMap {
+   public:
+    static_assert(std::is_integral_v<Key>, "ConcurrentMap supports only integer keys");
+
+    class Access {
+       public:
+        Value& ref_to_value;
+        Access(Value& ref_to_value, std::mutex& mutex) : ref_to_value(ref_to_value), guard_{mutex} {}
+
+       private:
+        std::lock_guard<std::mutex> guard_;
+    };
+
+    explicit ConcurrentMap(size_t bucket_count) : mutex_set_{bucket_count}, buckets_{bucket_count} {}
+
+    Access operator[](const Key& key) {
+        std::lock_guard<std::mutex> guard(mutex_);
+        size_t chank_idx = static_cast<size_t>(key) % buckets_.size();
+        return Access{buckets_[chank_idx][key], mutex_set_[chank_idx]};
+    }
+
+    std::map<Key, Value> BuildOrdinaryMap() {
+        std::map<Key, Value> result;
+        std::mutex mutex;
+        std::for_each(std::execution::seq, buckets_.begin(), buckets_.end(), [&result, &mutex](const auto& container) {
+            for (auto& [k, v] : container) {
+                auto value = v;
+                {
+                    std::lock_guard<std::mutex> guard(mutex);
+                    result[k] = std::move(value);
+                }
+            }
+        });
+        return result;
+    }
+
+   private:
+    std::vector<std::mutex> mutex_set_;
+    std::vector<std::map<Key, Value>> buckets_;
+    std::mutex mutex_;
+};
 
 using namespace std;
 
-static string GenerateWord(mt19937& generator, int max_length) {
-    const int length = uniform_int_distribution(1, max_length)(generator);
-    string word;
-    word.reserve(length);
-    for (int i = 0; i < length; ++i) {
-        word.push_back(uniform_int_distribution('a', 'z')(generator));
-    }
-    return word;
-}
+void RunConcurrentUpdates(ConcurrentMap<int, int>& cm, size_t thread_count, int key_count) {
+    auto kernel = [&cm, key_count](int seed) {
+        vector<int> updates(key_count);
+        iota(begin(updates), end(updates), -key_count / 2);
+        shuffle(begin(updates), end(updates), mt19937(seed));
 
-template <template <typename, typename...> typename Container = vector, typename T = string>
-static Container<T> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
-    vector<T> words;
-    words.reserve(word_count);
-    for (int i = 0; i < word_count; ++i) {
-        words.push_back(GenerateWord(generator, max_length));
-    }
-    return Container(words.begin(), words.end());
-}
-
-template <typename Strings, typename Predicate, typename Function>
-void Test(string_view mark, const Strings& strings, Predicate predicate, Function function) {
-    LOG_DURATION(mark);
-    const auto result = function(strings, predicate);
-    cout << result.size() << " " << result[5].substr(0, 5) << endl;
-}
-
-#define TEST(function) Test(#function, strings, predicate, function<vector<string>, decltype(predicate)>)
-
-template <typename Container, typename Predicate>
-vector<typename Container::value_type> CopyIfUnorderedSync(const Container& container, Predicate predicate) {
-    vector<typename Container::value_type> result;
-    for (const auto& value : container) {
-        if (predicate(value)) {
-            result.push_back(value);
-        }
-    }
-    return result;
-}
-
-template <typename Container, typename Predicate>
-vector<typename Container::value_type> CopyIfUnordered(const Container& container, Predicate predicate) {
-    atomic_size_t size {0};
-    vector<typename Container::value_type> result(container.size());
-    std::for_each(std::execution::par, container.begin(), container.end(), [&result, predicate, &size](const auto& value) {
-        if (predicate(value)) {
-            typename Container::value_type* destination;
-            destination = &result[size++];
-            *destination = value;
-        }
-    });
-    result.resize(size);
-    return result;
-}
-
-template <typename Container, typename Predicate>
-vector<typename Container::value_type> CopyIfUnorderedYandex(const Container& container, Predicate predicate) {
-    vector<typename Container::value_type> result(container.size());
-    atomic_int size = 0;
-    for_each(
-            execution::par,
-            container.begin(), container.end(),
-            [predicate, &size, &result](const auto& value) {
-                if (predicate(value)) {
-                    result[size++] = value;
-                }
+        for (int i = 0; i < 2; ++i) {
+            for (auto key : updates) {
+                ++cm[key].ref_to_value;
             }
-    );
-    result.resize(size);
-    return result;
+        }
+    };
+
+    vector<future<void>> futures;
+    for (size_t i = 0; i < thread_count; ++i) {
+        futures.push_back(async(kernel, i));
+    }
+}
+
+void RunConcurrentUpdatesSync(ConcurrentMap<int, int>& cm, size_t thread_count, int key_count) {
+    for (size_t i = 0; i < thread_count; ++i) {
+        vector<int> updates(key_count);
+        iota(begin(updates), end(updates), -key_count / 2);
+        shuffle(begin(updates), end(updates), mt19937(i));
+
+        for (int i = 0; i < 2; ++i) {
+            for (auto key : updates) {
+                ++cm[key].ref_to_value;
+            }
+        }
+    }
+}
+
+void TestConcurrentUpdate() {
+    constexpr size_t THREAD_COUNT = 3;
+    constexpr size_t KEY_COUNT = 50000;
+
+    ConcurrentMap<int, int> cm(THREAD_COUNT);
+    RunConcurrentUpdates(cm, THREAD_COUNT, KEY_COUNT);
+
+    const auto result = cm.BuildOrdinaryMap();
+    ASSERT_EQUAL(result.size(), KEY_COUNT);
+    for (auto& [k, v] : result) {
+        AssertEqual(v, 6, "Key = " + to_string(k));
+    }
+}
+
+void TestConcurrentUpdateSync() {
+    constexpr size_t THREAD_COUNT = 3;
+    constexpr size_t KEY_COUNT = 50000;
+
+    ConcurrentMap<int, int> cm(THREAD_COUNT);
+    RunConcurrentUpdates(cm, THREAD_COUNT, KEY_COUNT);
+
+    const auto result = cm.BuildOrdinaryMap();
+    ASSERT_EQUAL(result.size(), KEY_COUNT);
+    for (auto& [k, v] : result) {
+        AssertEqual(v, 6, "Key = " + to_string(k));
+    }
+}
+
+void TestReadAndWrite() {
+    ConcurrentMap<size_t, string> cm(5);
+
+    auto updater = [&cm] {
+        for (size_t i = 0; i < 50000; ++i) {
+            cm[i].ref_to_value.push_back('a');
+        }
+    };
+    auto reader = [&cm] {
+        vector<string> result(50000);
+        for (size_t i = 0; i < result.size(); ++i) {
+            result[i] = cm[i].ref_to_value;
+        }
+        return result;
+    };
+
+    auto u1 = async(updater);
+    auto r1 = async(reader);
+    auto u2 = async(updater);
+    auto r2 = async(reader);
+
+    u1.get();
+    u2.get();
+
+    for (auto f : {&r1, &r2}) {
+        auto result = f->get();
+        ASSERT(all_of(result.begin(), result.end(), [](const string& s) {
+            return s.empty() || s == "a" || s == "aa";
+        }));
+    }
+}
+
+void TestSpeedup() {
+    {
+        ConcurrentMap<int, int> single_lock(1);
+
+        LOG_DURATION("Single lock");
+        RunConcurrentUpdates(single_lock, 4, 50000);
+    }
+    {
+        ConcurrentMap<int, int> many_locks(100);
+
+        LOG_DURATION("100 locks");
+        RunConcurrentUpdates(many_locks, 4, 50000);
+    }
 }
 
 int main() {
-    vector<int> numbers(1'000);
-    iota(numbers.begin(), numbers.end(), 0);
-
-    const vector<int> even_numbers = CopyIfUnordered(numbers, [](int number) {
-        return number % 2 == 0;
-    });
-    for (const int number : even_numbers) {
-        cout << number << " "s;
-    }
-    cout << endl;
-    // выведет все чётные числа от 0 до 999
-
-    mt19937 generator;
-
-    const auto strings = GenerateDictionary<vector>(generator, 500'000, 3000);
-    auto predicate = [](const string& s) {
-        return count(s.begin(), s.end(), 'a') < 100;
-    };
-
-    
-    TEST(CopyIfUnorderedYandex);
-    TEST(CopyIfUnordered);
-    TEST(CopyIfUnorderedSync);
+    TestRunner tr;
+    // RUN_TEST(tr, TestConcurrentUpdateSync);
+    RUN_TEST(tr, TestConcurrentUpdate);
+    RUN_TEST(tr, TestReadAndWrite);
+    RUN_TEST(tr, TestSpeedup);
+    /*
+        ConcurrentMap<int, int> cm{4};
+        cm[0].ref_to_value = 1;
+        cm[1].ref_to_value = 2;
+        cm[2].ref_to_value = 3;
+        cm[3].ref_to_value = 4;*/
 }
