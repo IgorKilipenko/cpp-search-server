ea22c6fbf22a7598d4bf44aa996fca6d71f9f493
IgorKilipenko <igorkili@mail.ru>
2022-07-27 01:26:50 +0700

    add ConcurrentSet nfix

--- search-server/concurrent_map.h
+++ search-server/concurrent_map.h
@@ -1,5 +1,8 @@
+#include <atomic>
+#include <cstddef>
 #include <execution>
 #include <map>
+#include <set>
 #include <mutex>
 #include <string>
 #include <vector>
@@ -35,10 +38,69 @@ class ConcurrentMap {
     std::map<Key, Value> BuildOrdinaryMap() {
         std::map<Key, Value> result;
         for (auto& [mutex, container] : buckets_) {
+            std::lock_guard<std::mutex> guard(mutex);
+            result.insert(container.begin(), container.end());
+        }
+        return result;
+    }
+
+    void Erase(const Key &key)
+    {
+        auto &bucket = buckets_[static_cast<size_t>(key) % buckets_.size()];
+        std::lock_guard<std::mutex> lock(bucket.mutex);
+        bucket.map.erase(key);
+    }
+
+    size_t Size() const { 
+        std::atomic_size_t result = 0;
+        for (auto& [_, container] : buckets_) {
+            //std::lock_guard<std::mutex> guard(mutex);
+            result += container.size();
+        }
+        return result;
+    }
+
+   private:
+    std::vector<Bucket> buckets_;
+};
+
+template <typename Key>
+class ConcurrentSet {
+   public:
+    static_assert(std::is_integral_v<Key>, "ConcurrentMap supports only integer keys");
+    struct Bucket {
+        std::mutex mutex;
+        std::set<Key> set;
+    };
+    class Access {
+       private:
+        std::lock_guard<std::mutex> guard_;
+
+       public:
+        Key& ref_to_value;
+        Access(const Key& key, Bucket& bucket) : guard_{bucket.mutex}, ref_to_value{bucket[key]} {}
+    };
+
+    explicit ConcurrentSet(size_t bucket_count) : buckets_{bucket_count} {}
+
+    Access operator[](const Key& key) {
+        return Access{key, buckets_[static_cast<size_t>(key) % buckets_.size()]};
+    }
+
+    std::set<Key> BuildOrdinaryMap() {
+        std::set<Key> result;
+        for (auto& [mutex, container] : buckets_) {
             std::lock_guard guard(mutex);
             result.insert(container.begin(), container.end());
         }
         return result;
+    }
+
+    void Erase(const Key &key)
+    {
+        auto &bucket = buckets_[static_cast<size_t>(key) % buckets_.size()];
+        std::lock_guard<std::mutex> lock(bucket.mutex);
+        bucket.set.erase(key);
     }
 
    private:
