f58c001daaf8af5ad5212253abb9eab94c75592e
IgorKilipenko <igorkili@mail.ru>
2022-08-01 01:37:43 +0700

    start solve sprint9 training: CheckTreeProperty (1.1 tree nodes)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,37 +1,83 @@
-#include <algorithm>
-#include <cstddef>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <cassert>
 
-using namespace std;
-
-struct Circle {
-    double x;
-    double y;
-    double r;
+template <typename T>
+struct TreeNode {
+    T value;
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
 };
 
-struct Dumbbell {
-    Circle circle1;
-    Circle circle2;
-    string text;
-};
-
-struct DumbbellHash {
-    size_t operator()(const Dumbbell& val) const {
-        vector<double> vals = {val.circle1.x, val.circle1.y, val.circle1.r, val.circle2.x, val.circle2.y, val.circle2.r};
-        size_t result = 0;
-        std::for_each(vals.begin(), vals.end(), [&](const double val) {
-            result = result * 37 + static_cast<size_t>(val*100);
-        }); 
-        result += std::hash<std::string>{}(val.text);
-        return result;
+template <typename T>
+void DeleteTree(TreeNode<T>* node) {
+    if (!node) {
+        return;
     }
-};
+    DeleteTree(node->left);
+    DeleteTree(node->right);
+    delete node;
+}
+/*
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node) {
+    if (!node->left && !node->right) {
+        return true;
+    }
+    if (node->left && node->value < node->left->value) {
+        return false;
+    }
+    if (node->right && node->right->value < node->value) {
+        return false;
+    }
+    if (node->left && node->right && node->right->value < node->left->value) {
+        return false;
+    }
+
+    bool reult = CheckTreeProperty(node->left);
+    reult = reult && CheckTreeProperty(node->right);
+
+    return reult;
+}*/
+
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node, const T* min, const T* max) {
+    if (min && node->value < *min) {
+        return false;
+    }
+    if (max && *max < node->value) {
+        return false;
+    }
+    if (!node->left && !node->right) {
+        return true;
+    }
+    bool result = node->left == nullptr;
+    result = !result &&
+             CheckTreeProperty(
+                 node->left, node->left->left ? &(node->left->left->value) : nullptr, node->left->right ? &(node->left->right->value) : nullptr);
+    
+    if (!result) {
+        return false;
+    }
+
+    result = node->right == nullptr;
+    result = !result &&
+             CheckTreeProperty(
+                 node->right, node->right->left ? &(node->right->left->value) : nullptr, node->right->right ? &(node->right->right->value) : nullptr);
+    return result;
+}
+
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node) {
+    return CheckTreeProperty<T>(node, nullptr, nullptr);
+}
 
 int main() {
-    DumbbellHash hash;
-    Dumbbell dumbbell{{10, 11.5, 2.3}, {3.14, 15, -8}, "abc"s};
-    cout << "Dumbbell hash "s << hash(dumbbell);
+    using T = TreeNode<int>;
+    T* root1 = new T{6, new T{4, new T{3}, new T{5}}, new T{7}};
+    assert(CheckTreeProperty(root1));
+
+    T* root2 = new T{6, new T{4, new T{3}, new T{5}}, new T{7, new T{8}}};
+    assert(!CheckTreeProperty(root2));
+
+    DeleteTree(root1);
+    DeleteTree(root2);
 }
