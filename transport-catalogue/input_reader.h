8e9ff012a15afbd825ebcb12ca4ed156164985d5
IgorKilipenko <igorkili@mail.ru>
2022-08-04 06:05:11 +0700

    add PorccessRequests

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <iostream>
 #include <memory>
@@ -9,43 +11,60 @@
 #include <vector>
 
 #include "geo.h"
+#include "transport_catalogue.h"
 
 namespace transport_catalogue::io::detail {
-    using namespace std::literals;
-
     size_t TrimStart(std::string_view& str, const char ch = ' ');
     size_t TrimEnd(std::string_view& str, const char ch = ' ');
     void Trim(std::string_view& str, const char ch = ' ');
     std::vector<std::string_view> SplitIntoWords(std::string_view str, const char ch = ' ');
+}
 
-    class CommandParser {
+namespace transport_catalogue::io {
+    using namespace std::literals;
+
+    class Parser {
         using Coordinates = geo::Coordinates;
         using StopsContainer = std::unordered_map<std::string_view, Coordinates>;
 
     public:
+        using StopCmdResult = std::pair<std::string_view, Coordinates>;
+        struct RawRequest {
+            std::string_view command;
+            std::string_view value;
+            std::string_view args;
+
+            RawRequest() = default;
+            RawRequest(std::string_view command, std::string_view value, std::string_view args) : command{command}, value{value}, args{args} {}
+        };
         struct Names {
             static constexpr const std::string_view STOP = "Stop"sv;
             static constexpr const std::string_view BUS = "Bus"sv;
         };
-        bool Parse(const std::string_view raw_cmd) {
+        std::shared_ptr<StopCmdResult> ParseStopCmd(const std::string_view raw_cmd) const {
             if (raw_cmd.empty()) {
-                return false;
+                return nullptr;
             }
             std::string_view cmd = raw_cmd;
-            Trim(cmd);
-            if (IsAddStopCmd(cmd)) {
-                cmd.remove_prefix(Names::STOP.size());
-                TrimStart(cmd);
-                std::cout << "IS STOP command" << std::endl;
-                auto key_val_ptr = SplitKeyValue(cmd);
-                if (key_val_ptr) {
-                    stops_.emplace(key_val_ptr->first, ParseLatLng(key_val_ptr->second)); 
-                    return true;
-                }
+            // Trim(cmd);
+            assert(IsStopCmd(cmd));
+
+            cmd.remove_prefix(Names::STOP.size());
+            detail::TrimStart(cmd);
+
+            std::cout << "IS STOP command" << std::endl;
+
+            auto key_val_ptr = SplitKeyValue(cmd);
+            if (key_val_ptr) {
+                // stops_.emplace(key_val_ptr->first, ParseLatLng(key_val_ptr->second));
+                StopCmdResult result{key_val_ptr->first, ParseLatLng(key_val_ptr->second)};
+                return std::make_shared<StopCmdResult>(std::move(result));
             }
-            return false;
+
+            return nullptr;
         }
-        static std::shared_ptr<std::pair<std::string_view, std::string_view>> SplitKeyValue(const std::string_view str) {
+
+        std::shared_ptr<std::pair<std::string_view, std::string_view>> SplitKeyValue(const std::string_view str) const {
             size_t idx = str.find(':');
             if (idx == std::string::npos) {
                 return nullptr;
@@ -57,14 +76,22 @@ namespace transport_catalogue::io::detail {
             std::pair<std::string_view, std::string_view> result{key, value};
             return std::make_shared<std::pair<std::string_view, std::string_view>>(std::move(result));
         }
-        static Coordinates ParseLatLng(std::string_view str) {
+
+        RawRequest SplitRequest(const std::string_view str) const {
+            auto key_val_ptr = SplitKeyValue(str);
+            assert(key_val_ptr != nullptr);
+            auto strings = detail::SplitIntoWords(key_val_ptr->first);
+            assert(strings.size() == 2);
+            return RawRequest(strings[0], strings[1], key_val_ptr->second);
+        }
+
+        Coordinates ParseLatLng(std::string_view str) const {
             return {};
         }
 
-        static bool IsAddStopCmd(const std::string_view cmd) {
+        bool IsStopCmd(const std::string_view cmd) const {
             return Names::STOP == cmd.substr(0, Names::STOP.size());
         }
-
         const StopsContainer& GetStops() const {
             return stops_;
         }
@@ -72,22 +99,52 @@ namespace transport_catalogue::io::detail {
     private:
         StopsContainer stops_;
     };
-}
 
-namespace transport_catalogue::io {
     class Reader {
     public:
-        Reader(std::istream& in_stream = std::cin) : in_stream_{in_stream} {}
+        struct Request {
+            enum class RequestType { ADD, REMOVE };
+            RequestType type = RequestType::ADD;
+            Stop value;
+        };
+        Reader(TransportCatalogue& catalog, std::istream& in_stream = std::cin) : in_stream_{in_stream}, catalog_{catalog} {}
         template <typename TOut = std::string>
-        TOut Read();
+        TOut Read() {
+            TOut result;
+            in_stream_ >> result;
+            in_stream_.get();
+            return result;
+        }
         std::string ReadRawLine();
-        std::vector<std::string> ReadAllInputLines();
-        void PraseCommand(const std::string_view raw_cmd) const {
-            cmd_parser_.Parse(raw_cmd);
+        std::vector<std::string> ReadLines(size_t count) {
+            std::vector<std::string> result{count};
+            int idx = 0;
+            for (std::string line; idx < count && std::getline(in_stream_, line); ++idx) {
+                result[idx] = line;
+            }
+            return result;
+        }
+        void PraseRequest(const std::string_view raw_cmd) {
+            std::string_view cmd = raw_cmd;
+            detail::Trim(cmd);
+            assert(!cmd.empty());
+            auto raw_req = parser_.SplitRequest(cmd);
+            if (parser_.IsStopCmd(raw_req.command)) {
+                auto stop_result_ptr = parser_.ParseStopCmd(raw_cmd);
+                catalog_.AddStop(static_cast<std::string>(stop_result_ptr->first), std::move(stop_result_ptr->second));
+            }
+        }
+        void PorccessRequests() {
+            size_t n = Read<size_t>();
+            auto lines = ReadLines(n);
+            std::for_each(lines.begin(), lines.end(), [&](const std::string_view raw_cmd) {
+                PraseRequest(raw_cmd);
+            });
         }
 
     private:
         std::istream& in_stream_;
-        io::detail::CommandParser cmd_parser_;
+        Parser parser_;
+        TransportCatalogue catalog_;
     };
 }
--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -9,20 +9,10 @@ using namespace transport_catalogue;
 
 int main() {
     std::cout << "Start..." << std::endl;
-    io::Reader reader(std::cin);
     TransportCatalogue catalog;
-    std::string cur_line;
-    while (!(cur_line = reader.ReadRawLine()).empty()) {
-        std::cout << "> " << cur_line << std::endl;
-        reader.PraseCommand(cur_line);
-    }
-
+    io::Reader reader(catalog, std::cin);
     
-    catalog.AddStop("TestStop", geo::Coordinates{99.55, 88.55});
-    geo::Coordinates coords{99.55, 88.55};
-    catalog.AddStop("TestStop2", std::move(coords));
-    const geo::Coordinates coords2{89.55, 88.55};
-    catalog.AddStop("TestStop3", coords2);
+    reader.PorccessRequests();
 
     return 0;
 }
