2702265c2a22b6ca5488f56beffb148e69bd4c11
IgorKilipenko <igorkili@mail.ru>
2022-08-05 06:17:34 +0700

    add get queries proccess

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -12,11 +12,11 @@
 using namespace transport_catalogue;
 
 int main() {
-    std::cout << "Start..." << std::endl;
+    //std::cout << "Start..." << std::endl;
     // auto db = std::make_shared<TransportCatalogue::Database>(TransportCatalogue::Database());
     std::stringstream ss;
     {
-        std::vector<std::string> input_lines{
+        std::vector<std::string> input_add_queries{
             "Stop Tolstopaltsevo:  55.611087  ,     37.208290   ",
             "Stop Marushkino: 55.595884, 37.209755",
             "Bus 256: Biryulyovo Zapadnoye > Biryusinka > Universam > Biryulyovo Tovarnaya > Biryulyovo Passazhirskaya > Biryulyovo Zapadnoye",
@@ -28,22 +28,35 @@ int main() {
             "Stop Biryulyovo Tovarnaya: 55.592028, 37.653656",
             "Stop Biryulyovo Passazhirskaya: 55.580999, 37.659164",
         };
-        ss << input_lines.size() << '\n';
-        std::for_each(input_lines.begin(), input_lines.end(), [&](const std::string_view line) {
+
+        std::vector<std::string> input_get_queries{
+            "Bus 256",
+            "Bus 750",
+            "Bus 751",
+        };
+
+        ss << input_add_queries.size() << '\n';
+        std::for_each(input_add_queries.begin(), input_add_queries.end(), [&](const std::string_view line) {
+            ss << line << '\n';
+        });
+
+        ss << input_get_queries.size() << '\n';
+        std::for_each(input_get_queries.begin(), input_get_queries.end(), [&](const std::string_view line) {
             ss << line << '\n';
         });
     }
+
     std::shared_ptr<TransportCatalogue::Database> db_ptr = nullptr;
     {
         TransportCatalogue catalog;
-        const io::Reader reader(*catalog.GetDatabase(), ss);
+        const io::Reader reader(*catalog.GetDatabaseForWrite(), ss);
 
         reader.PorccessRequests();
 
-        db_ptr = catalog.GetDatabase();
+        db_ptr = catalog.GetDatabaseForWrite();
         auto& db = *db_ptr;
-        [[maybe_unused]]const auto& stop = db.GetStopsTable();
-        [[maybe_unused]]const auto& bus = db.GetBusRoutesTable();
+        [[maybe_unused]] const auto& stop = db.GetStopsTable();
+        [[maybe_unused]] const auto& bus = db.GetBusRoutesTable();
     }
 
     return 0;
