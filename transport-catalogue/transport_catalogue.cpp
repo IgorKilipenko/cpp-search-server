e7a20fbf63f669a154d3fa0726a8bd750a9de94a
IgorKilipenko <igorkili@mail.ru>
2022-08-05 04:36:29 +0700

    split by files

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,4 +1,14 @@
 #include "transport_catalogue.h"
 namespace transport_catalogue {
+    const std::deque<Stop>& TransportCatalogue::GetStops() const {
+        return db_->GetStopsTable();
+    }
 
+    const std::shared_ptr<TransportCatalogue::Database> TransportCatalogue::GetDatabaseForWrite() const {
+        return db_;
+    }
+
+    const std::shared_ptr<const TransportCatalogue::Database> TransportCatalogue::GetDatabaseReadOnly() const {
+        return db_;
+    }
 }
