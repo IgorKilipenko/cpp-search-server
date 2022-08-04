216b3bbbf4cc73f86dd3ea06d1dee3b6f6f400a5
IgorKilipenko <igorkili@mail.ru>
2022-08-04 09:06:34 +0700

    mig to stringstream

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <istream>
 #include <memory>
+#include <sstream>
 
 #include "geo.h"
 #include "input_reader.h"
@@ -11,10 +12,16 @@ using namespace transport_catalogue;
 int main() {
     std::cout << "Start..." << std::endl;
     //auto db = std::make_shared<TransportCatalogue::Database>(TransportCatalogue::Database());
+    std::stringstream ss;
+    {
+        ss << "2" << '\n'; 
+        ss << "Stop Tolstopaltsevo: 55.611087, 37.208290" << '\n';
+        ss << "Stop Marushkino: 55.595884, 37.209755" << std::endl;
+    }
     std::shared_ptr<TransportCatalogue::Database> db = nullptr;
     {
         TransportCatalogue catalog;
-        io::Reader reader(*catalog.GetDatabase(), std::cin);
+        io::Reader reader(*catalog.GetDatabase(), ss);
 
         reader.PorccessRequests();
 
